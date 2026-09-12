0f57c14a827896415d61ef1f704c41225c647772
Dharmaraj R <dharmaraj.171215@gmail.com>
2026-09-12 12:26:15 +0530

    refactor(script): improve know-project-status.sh for better project handling and structure

--- helpers/know-project-status.sh
+++ helpers/know-project-status.sh
@@ -1,19 +1,35 @@
-# Get project name argument from the command line
+#!/bin/sh
+
 PROJECT_NAME="${1}"
 
+
+IGNORE_LIST="template-react-microservice template-springboot-microservice template-python-microservice template-nodejs-microservice"
+
+issues() {
+    name="$1"
+    gh issue list --repo Klyptr-Studio/"$name"
+    echo ""
+}
+
+pull_requests() {
+    name="$1"
+    gh pr list --repo Klyptr-Studio/"$name"
+    echo ""
+}
+
 if [ -z "$PROJECT_NAME" ]; then
-    echo "Error: Project name argument is missing."
-    echo "Usage: ./know-project-status.sh <project-name>"
-    exit 1
+    REPO_LIST=$(gh repo list Klyptr-Studio --limit 100 | awk 'NR > 1 {split($1, parts, "/"); print parts[2]}')
+
+    for repo in $REPO_LIST; do
+        case " $IGNORE_LIST " in
+            *" $repo "*) continue ;;
+        esac
+
+            issues "$repo"
+            pull_requests "$repo"
+    done
+else
+    issues "$PROJECT_NAME"
+    pull_requests "$PROJECT_NAME"
 fi
 
-echo "======================================"
-echo "=============== ISSUES ==============="
-echo "======================================"
-gh issue list --repo Klyptr-Studio/"$PROJECT_NAME"
-echo ""
-
-echo "======================================"
-echo "============ PULL REQUESTS ==========="
-echo "======================================"
-gh pr list --repo Klyptr-Studio/"$PROJECT_NAME"
